a9ed6596cbd72ae3f8fd06069f142daf437135e7
bluerain4 <code.maito@gmail.com>
2021-07-29 08:51:07 +0700

    creating customer

--- src/main/java/com/example/usermanage/controller/EmployeeController.java
+++ src/main/java/com/example/usermanage/controller/EmployeeController.java
@@ -4,12 +4,11 @@ import com.example.usermanage.model.Employee;
 import com.example.usermanage.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -19,6 +18,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 
     @GetMapping("/cc")
