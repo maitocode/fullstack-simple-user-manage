44f4958d3127e50c89c411eaccc72c627b230162
bluerain4 <code.maito@gmail.com>
2021-07-29 08:50:02 +0700

    finish create customer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ListEmployeeComponent from "./components/ListEmployeeComponent.jsx";
+import HeaderComponent from "./components/HeaderComponent.jsx";
+import FooterComponent from "./components/FooterComponent.jsx";
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={ListEmployeeComponent}></Route>
+            <Route path="/employees" component={ListEmployeeComponent}></Route>
+            <Route
+              path="/add-employee"
+              component={CreateEmployeeComponent}
+            ></Route>
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
